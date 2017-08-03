efa56a3f57b16bfe32a619a6e19f99087ed80ed5
Astri Jeff Chung <jeffchung@astri.com>
2017-08-03 14:38:23 +0800

    fix

--- make/lib/prod.js
+++ make/lib/prod.js
@@ -6,11 +6,13 @@ import uglify from 'rollup-plugin-uglify';
 import config from '../app/dev';
 
 // Inject the production settings.
+config.entry = 'src/lib.js';
 config.dest = 'build/lib/index.min.js';
-// config.external = ['react', 'react-dom'];
-// config.plugins[3] = replace({
-//   'process.env.NODE_ENV': JSON.stringify('production')
-// });
-// config.plugins.push(uglify());
+config.format = 'cjs';
+config.external = ['react', 'react-dom'];
+config.plugins[3] = replace({
+  'process.env.NODE_ENV': JSON.stringify('production')
+});
+config.plugins.push(uglify());
 
 export default config;

1571efc013dd1cc1c580ba99e42ec41048d662a5
Astri Jeff Chung <jeffchung@astri.com>
2017-08-03 15:13:00 +0800

    fix

--- src/lib.js
+++ src/lib.js
@@ -2,9 +2,13 @@ import * as Action from 'action';
 import Customer from 'component/Customer';
 import Price from 'component/Price';
 import Product from 'component/Product';
+import customerReducer from 'reducer/customer';
+import sandWichNoReducer from 'reducer/sandWichNo';
 export default {
   Action,
   Customer,
   Price,
-  Product
+  Product,
+  customerReducer,
+  sandWichNoReducer
 };
